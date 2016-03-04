28fc409580c30703d5d7a846d363a4bae115636b
Austin Rose <austin@percipientnetworks.com>
2016-03-04 18:36:18 -0500

    .

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-sudo apt-get install -Y git
-sudo apt-get install -Y python-pip
-sudo pip install virtualenv
-sudo pip install django
-git clone https://github.com/austintrose/quick-django-proxy.git
+sudo apt-get install -y git;
+sudo apt-get install -y python-pip;
+sudo pip install virtualenv;
+sudo pip install django;
+git clone https://github.com/austintrose/quick-django-proxy.git;
 sudo python quick-django-proxy/quick_django_proxy/manage.py runserver 0.0.0.0:80
